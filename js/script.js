8e5effa641e83fcf284d1505fced642e0b5e7fec
Admin <rizki.achanation@gmail.com>
2025-04-11 18:03:23 +0700

    adding assets/img and updates

---
+++ js/script.js
@@ -0,0 +1,5 @@
+const toggle = document.getElementById('modeToggle');
+toggle.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+    toggle.textContent = document.body.classList.contains('dark-mode') ? '☀️' : '🌙';
+});
