7f8a71c026dd8f8257f0580427618d7aaef19c7f
Admin <rizki.achanation@gmail.com>
2025-04-13 12:37:41 +0700

    adding img and updates

--- js/script.js
+++ js/script.js
@@ -1,47 +1,46 @@
-const toggleBtn = document.getElementById("modeToggle");
+const toggleBtn = document.getElementById('modeToggle');
 const toggler = document.getElementById('navbarToggleBtn');
 const togglerIcon = document.getElementById('toggleIcon');
 const body = document.body;
 
-  if (localStorage.getItem("theme") === "dark") {
-    body.classList.add("dark-mode");
-  }
+if (localStorage.getItem('theme') === 'dark') {
+  body.classList.add('dark-mode');
+}
 
-  toggleBtn.addEventListener("click", () => {
-    body.classList.toggle("dark-mode");
-    const isDark = body.classList.contains("dark-mode");
-    localStorage.setItem("theme", isDark ? "dark" : "light");
+toggleBtn.addEventListener('click', () => {
+  body.classList.toggle('dark-mode');
+  const isDark = body.classList.contains('dark-mode');
+  localStorage.setItem('theme', isDark ? 'dark' : 'light');
 });
 
 toggler.addEventListener('click', function () {
-    this.classList.toggle('open');
-    if (this.classList.contains('open')) {
-        toggleIcon.src = "./assets/img/x-toggle.svg";
-      } else {
-        toggleIcon.src = "./assets/img/burger-toggle.svg"; 
-      }
-  });
+  this.classList.toggle('open');
+  if (this.classList.contains('open')) {
+    toggleIcon.src = './assets/img/x-toggle.svg';
+  } else {
+    toggleIcon.src = './assets/img/burger-toggle.svg';
+  }
+});
 
-  function updateNavbar() {
-    const navbar = document.querySelector('.navbar');
-    if (window.scrollY <= 70) {
-        navbar.classList.add('scrolled');
-    } else {
-        navbar.classList.remove('scrolled');
-    }
+function updateNavbar() {
+  const navbar = document.querySelector('.navbar');
+  if (window.scrollY <= 70) {
+    navbar.classList.add('scrolled');
+  } else {
+    navbar.classList.remove('scrolled');
+  }
 }
 
 window.addEventListener('scroll', updateNavbar);
 
-
 document.addEventListener('DOMContentLoaded', updateNavbar);
 
-document.querySelectorAll('a[href="#home"]').forEach(link => {
-    link.addEventListener('click', function(e) {
-        e.preventDefault();
-        window.scrollTo({
-            top: 0,
-            behavior: 'smooth'
-        });
+document.querySelectorAll('a[href="#home"]').forEach((link) => {
+  link.addEventListener('click', function (e) {
+    e.preventDefault();
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
     });
+  });
 });
